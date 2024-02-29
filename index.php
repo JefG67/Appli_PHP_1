5f596d072154629f28270b1cf6d4bef9b15e9652
JefG67 <jefguilpain@gmail.com>
2024-02-29 15:09:37 +0100

    integrer ma function a mon index pour le renvoyer dans le bouton

--- index.php
+++ index.php
@@ -8,34 +8,62 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Ajout produit</title>
 </head>
-<div class="d-flex flex-column align-items-center m-5">
+
 <body>
-    <h1 class="text-primary">Ajouter un produit</h1>
-    <form action="traitement.php" method="post">
-        
-        <p>
-            <label>
-                Nom du produit :
-                <input type="text" name="name" class="form-control mx-3">
-            </label>
+    <div class="d-flex flex-column align-items-center m-5">
+
         </p>
-        <p>
-            <label>
-                Prix du produit en € :
-                <input type="number" step="any" name="price" class="form-control mx-3">
-            </label>
-        </p>
-        <p>
-            <label>
-                Quantité désirée :
-                <input type="number" name="qtt" value="1" class="form-control mx-3">
-            </label>
-        </p>
-        <p>
-            <input type="submit" name="submit" value="Ajouter le produit" class="btn btn-primary mx-auto text-center">
-        </p>
-    </form>
- </body>
+        <h1 class="text-primary">Ajouter un produit</h1>
+        <form action="traitement.php" method="post">
+
+            <p>
+                <label>
+                    Nom du produit :
+                    <input type="text" name="name" class="form-control mx-3">
+                </label>
+            </p>
+            <p>
+                <label>
+                    Prix du produit en € :
+                    <input type="number" step="any" name="price" class="form-control mx-3">
+                </label>
+            </p>
+            <p>
+                <label>
+                    Quantité désirée :
+                    <input type="number" name="qtt" value="1" class="form-control mx-3">
+                </label>
+            </p>
+            <p>
+            <div class="text-center">
+                <input type="submit" name="submit" value="Ajouter le produit" class="btn btn-primary mx-auto text-center">
+                <div class="position-absolute top-0"style="margin-top: 10px;">
+                    <input type="submit" name="submit" value="Ajouter le produit" class="btn btn-primary mx-auto text-center">
+                    <a href="http://localhost/Guilpain%20Jean-Fran%C3%A7ois/Appli_PHP_1/recap.php" class="btn btn-primary position-relative">
+                        Panier
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                            99+
+                            <span class="visually-hidden">unread messages</span>
+                        </span>
+                    </a>
+                </div>
+            </div>
+            </p>
+        </form>
+</body>
 
 </html>
+
+<?php
+session_start();
+// je verifie si la variable existe et est non nul 
+if (isset($_SESSION['message'])) {
+    // envoyer le message puis le supprime ensuite 
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+}
+?>
+
+
+
 
--- traitement.php
+++ traitement.php
@@ -1,24 +1,29 @@
-<?php
+ <?php
     session_start();
 
-    if(isset($_POST['submit'])){
 
-            $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
-            $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
-            $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+    if (isset($_POST['submit'])) {
 
-            if($name && $price && $qtt){
-                $product = [
-                    "name" => $name,
-                    "price" => $price,
-                    "qtt" => $qtt,
-                    "total" => $price*$qtt,
-                
-                ];
-                $_SESSION['products'][] = $product;
+        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
+        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
 
-            }
+        if ($name && $price && $qtt) {
+            $product = [
+                "name" => $name,
+                "price" => $price,
+                "qtt" => $qtt,
+                "total" => $price * $qtt,
 
+            ];
+            $_SESSION['products'][] = $product;
+            //message a afficher quand l'ajout d'un produit c'est bien passé
+            $_SESSION['message'] = "<div class='alert alert-success' role='alert'>
+                Votre produit à bien été enrengistré !
+              </div>";
+        }
     }
 
     header("Location:index.php");
+
+ 
