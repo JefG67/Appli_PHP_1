3ee1bc89da246e433b4172ad9e167dd7db4e929f
JefG67 <jefguilpain@gmail.com>
2024-03-01 16:17:02 +0100

    comprendre le elseif pour ajouter l'alerte

--- traitement.php
+++ traitement.php
@@ -1,6 +1,6 @@
  <?php
     session_start();
-    
+
 
 
     if (isset($_POST['submit'])) {
@@ -22,9 +22,19 @@
             $_SESSION['message'] = "<div class='alert alert-success' role='alert'>
                 Votre produit à bien été enrengistré !
               </div>";
-        }
+        } elseif
+            (isset($_POST['submit'])){ //je verifie l'existence de la clé "submit" dans le tableau $_POST, la clé correspond à l'attribut "name" du bouton input type="submit" name="submit" du formulaire.La condition sera alors vraie seulement si la requete POST transmet bien une clé "submit" au serveur
+                
+                $_SESSION['Erreur'] = "<div class='alert alert-danger' role='alert'>
+                Votre produit n'a pas été enregistré ! 
+                </div>";   
+
+            }
+        
     }
 
     header("Location:index.php");
 
- 
+
+
+
